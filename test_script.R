95b8b8ab01617ddf2eaf7e46163bfe2b3898a354
katherineleyq <katherineleyq@gmail.com>
2024-08-12 19:48:14 +0000

    created 50 sec loop

--- test_script.R
+++ test_script.R
@@ -1,1 +1,19 @@
-# This is a test script
+# this script takes 50 seconds to run
+
+# record start time
+start_time = Sys.time()
+
+# create a loop that iterates 5 times and sets system to sleep for 10 seconds
+for (i in 1:5) {
+  cat("Iteration", i, "\n")
+  Sys.sleep(10)
+}
+
+# record end time
+end_time = Sys.time()
+
+# print total execution time
+total_time = as.numeric(difftime(end_time, start_time, units = "secs"))
+
+# write results to txt file
+write(paste("Total execution time: ", total_time, "seconds"), file = "execution_time.txt")
